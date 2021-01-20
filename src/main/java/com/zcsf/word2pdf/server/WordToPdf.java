1af1387c62a60ebf3fd6a354bd779dea461d6d29
よーぐると <17673051831@163.com>
2021-01-20 11:36:12 +0800

    init 调用python生成pdf

---
+++ src/main/java/com/zcsf/word2pdf/server/WordToPdf.java
@@ -0,0 +1,13 @@
+package com.zcsf.word2pdf.server;
+
+/**
+ * @Author ZXC
+ * @Date 2021/1/20 11:35
+ * @description
+ */
+public class WordToPdf {
+    //调用python生成pdf
+    public static void main(String[] args){
+
+    }
+}
