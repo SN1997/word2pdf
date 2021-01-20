d7aa0ef4569cac80a2501782d78c1d18c5b630a0
よーぐると <17673051831@163.com>
2021-01-20 11:24:44 +0800

    init project

---
+++ src/main/java/com/zcsf/word2pdf/Word2pdfApplication.java
@@ -0,0 +1,13 @@
+package com.zcsf.word2pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Word2pdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Word2pdfApplication.class, args);
+    }
+
+}
